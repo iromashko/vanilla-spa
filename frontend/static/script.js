094210bb0abe897249c12e0239402aa88a9ac000
iromashko <iromashko@me.com>
2020-11-09 03:04:57 +0300

    part 1

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,1 +1,28 @@
-console.log(`JS is loaded`);
+const router = async () => {
+  const routes = [
+    {
+      path: '/',
+      view: () => console.log(`Viewing Dashboard`),
+    },
+    {
+      path: '/posts',
+      view: () => console.log(`Viewing Posts`),
+    },
+    {
+      path: '/settings',
+      view: () => console.log(`Viewing Settings`),
+    },
+  ];
+
+  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+
+  const potentialMatches = routes.map((route) => {
+    return {
+      route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+  console.log(potentialMatches);
+};
+
+document.addEventListener('DOMContentLoaded', router);
